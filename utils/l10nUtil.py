497682f4365af8df56ef650cdbd221f03be73d87
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-08-17 08:29:44 +0200

    Try to get project

--- utils/l10nUtil.py
+++ utils/l10nUtil.py
@@ -232,6 +232,7 @@ crowdinFileIDs = {
 	"emoticons.pot": 176,
 	"emoticons.po": 176,
 	"emoticons.xliff": 178,
+	# goldwave
 	"goldwave.pot": 180,
 	"goldwave.po": 180,
 	"goldwave.xliff": 182,
@@ -287,6 +288,7 @@ def uploadSourceFile(localFilePath: str):
 	print(f"Importing source file {localFilePath} from storage with ID {storageId}")
 	res = getCrowdinClient().source_files.add_file(storageId=storageId, projectId=CROWDIN_PROJECT_ID, name=filename, title=title, exportOptions=exportOptions)
 	print("Done")
+	getCrowdinClient.projects.getProject(CROWDIN_PROJECT_ID)
 
 
 def uploadTranslationFile(crowdinFilePath: str, localFilePath: str, language: str):
